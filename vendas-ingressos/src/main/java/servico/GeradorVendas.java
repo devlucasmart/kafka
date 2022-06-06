b17ce131e9f8e808ce90b24cc2ca45bbef5037a9
Lucas Martins <lucas.martins.fsa@gmail.com>
2022-06-06 11:40:10 -0300

    estruturando projeto de vendas

---
+++ vendas-ingressos/src/main/java/servico/GeradorVendas.java
@@ -0,0 +1,12 @@
+package servico;
+
+public class GeradorVendas {
+	private static Random rand = new Random();
+	public static void main(String[] args) {
+		Venda venda = geraVenda();
+}
+
+	private static Venda geraVenda() {
+		// TODO Auto-generated method stub
+		return null;
+	}
